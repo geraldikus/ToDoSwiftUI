87c36aa15fd3f831d251278ee5c17a5eeb9445a2
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-07-21 12:04:58 +0400

    Initial commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Anton on 21.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
