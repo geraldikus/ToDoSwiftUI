4a50cc261f0dc3d06696eecd904e2b762fa60f0c
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-07-25 16:41:40 +0400

    Todos data go to Firebase

--- ToDoSwiftUI/ViewModels/NewItemsViewModel.swift
+++ ToDoSwiftUI/ViewModels/NewItemsViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
@@ -15,7 +17,30 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // Get current user ID
+        guard let uId = Auth.auth().currentUser?.uid else { return }
+        
+        // Create a model
+        let newID = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newID,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        // Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newID)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
