c503912611971b3b7bdeab8f47d56da4d63ec101
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-07-25 16:18:54 +0400

    Create new item view

--- ToDoSwiftUI/ViewModels/ToDoListViewModel.swift
+++ ToDoSwiftUI/ViewModels/ToDoListViewModel.swift
@@ -9,5 +9,7 @@ import Foundation
 /// ViewModel for list of items view
 /// Primary tab
 class ToDoListViewViewModel: ObservableObject {
+    @Published var showingNewItemView = false
+    
     init() {}
 }
--- ToDoSwiftUI/Views/NewItemsView.swift
+++ ToDoSwiftUI/Views/NewItemsView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct NewItemsView: View {
+    
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Item")
+                .font(.system(size: 32))
+                .bold()
+                .padding(.top, 20)
+            
+            Form {
+                // Name
+                TextField("Write a new task", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                // Due date
+                DatePicker("Due date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                // Button
+                TLButton(title: "Save", backgroung: .pink) {
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newItemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
+                }
+                .padding()
+            }
+            .alert(isPresented: $viewModel.showAlert) {
+                Alert(title: Text("Error"), message: Text("Please fill in all fields and select due date that is today or newer."))
+            }
+        }
     }
 }
 
 struct NewItemsView_Previews: PreviewProvider {
     static var previews: some View {
-        NewItemsView()
+        NewItemsView(newItemPresented: Binding(get: {
+            return true
+        }, set: { _ in
+            
+        }))
     }
 }
